d6672c300c48e6ff81ff52e26ea93f0c21204bd4
17-10-rashmi <142078930+17-10-rashmi@users.noreply.github.com>
2023-09-18 09:17:32 +0530

    Add files via upload

---
+++ C-Sharp-master/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Lab1 lab1 = new Lab1();
+            lab1.program1();
+            lab1.program2();
+            lab1.program3();
+            lab1.program4();
+            lab1.program5();
+            lab1.program6();
+            lab1.program7();
+            lab1.program8();
+            lab1.program9();
+            lab1.program10();
+            Lab2 lab2 = new Lab2();
+            lab2.Program1();
+            lab2.Program2();
+            lab2.Program3();
+            lab2.Program4();
+            lab2.Program5();
+            lab2.Program6();
+            lab2.Program7();
+            lab2.Program8();
+            lab2.Program9();
+            lab2.Program10();
+        }
+    }
+}
